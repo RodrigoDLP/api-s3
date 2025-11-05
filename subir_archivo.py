4f1ba728d0c70269e4544ad8abbbe29a19b1654c
RodrigoDLP <rodrigo.delapuente@utec.edu.pe>
2025-11-05 18:58:11 -0500

    Add files via upload

--- subir_archivo.py
+++ subir_archivo.py
@@ -1,10 +1,31 @@
 import boto3
 
+import base64
+import boto3
+
+def upload_base_64_to_s3(s3_bucket_name, s3_file_name, base_64_str):
+    s3 = boto3.resource('s3')
+    s3.Object(s3_bucket_name, s3_file_name).put(Body=base64.b64decode(base_64_str))
+    return (s3_bucket_name, s3_file_name)
+
+def lambda_handler(event, context);
+    nombre_bucket = event['body']['bucket']
+    filePath = event['body']['filePath']
+    fileContent = event['body']['fileContent']
+    upload_base_64_to_s3(nombre_bucket, filePath, fileContent)
+    return {
+        'statusCode': 200,
+        'message': 'File uploaded successfully'
+    }
+
+
+"""
 def lambda_handler(event, context):
     # Entrada (json)
     nombre_bucket = event['body']['bucket']
     nombre_directorio = event['body']['directoryName']
     ruta = event['body']['filePath']
+    contenido = event['body']['fileContent']
         
     # Proceso
     s3 = boto3.client('s3')
@@ -25,4 +46,4 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'message': 'File uploaded successfully'
-    }
+    }"""
